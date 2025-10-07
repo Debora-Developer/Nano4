4e7c8a9b474e8f7907ac9750e88b7ccacb0a0c02
Débora <deboracosta.designer@gmail.com>
2025-10-07 08:51:37 -0300

    feat: Initial Commit

---
+++ Nano4/Nano4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nano4
+//
+//  Created by Débora Costa on 07/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nano4/Nano4/Nano4App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano4App.swift
+//  Nano4
+//
+//  Created by Débora Costa on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Nano4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
